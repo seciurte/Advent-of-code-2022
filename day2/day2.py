7cd51e4342f303aacdab6a00780211b7636eb184
seciurte <seciurte@student.42.fr>
2022-12-03 16:14:07 +0100

    to finish day2

--- day2/day2.py
+++ day2/day2.py
@@ -1,32 +1,51 @@
 #!/bin/python3
 
-INPUT_FILE = "input.txt"
+INPUT_FILE	= "input.txt"
 
-OP_RPS		= {
-	"A": 1,
-	"B": 2,
-	"C": 3
-}
-PL_RPS		= {
-	"X": 1,
-	"Y": 2,
-	"Z": 3
+IS_BEATEN	= {
+	"rock": "paper",
+	"paper": "scissors",
+	"scissors": "rock"
 }
 
-PL_WIN_TO	= {
-	OP_RPS["A"]: PL_RPS["Y"],
-	OP_RPS["B"]: PL_RPS["Z"],
-	OP_RPS["C"]: PL_RPS["X"],
+OP_TO_RPS	= {
+	"A": "rock",
+	"B": "paper",
+	"C": "scissors"
 }
 
-WIN		= 6
-DRAW	= 3
-LOSE	= 0
+PL_TO_RPS	= {
+	"X": "rock",
+	"Y": "paper",
+	"Z": "scissors"
+}
+
+PL_TO_WDL	= {
+	"X": "win",
+	"Y": "draw",
+	"Z": "lose"
+}
+
+SCORES		= {
+	"rock": 1,
+	"paper": 2,
+	"scissors": 3,
+	"win": 6,
+	"draw": 3,
+	"lose": 0
+}
 
 def getPlayerScore(op, pl) :
 
-	if (OP_RPS[op] == PL_RPS[pl]) :
-		return DRAW + PL_RPS[pl]
+	if OP_TO_RPS[op] == PL_TO_RPS[pl] :
+		return SCORES["draw"] + SCORES[PL_TO_RPS[pl]]
+	elif PL_TO_RPS[pl] == IS_BEATEN[OP_TO_RPS[op]] :
+		return SCORES["win"] + SCORES[PL_TO_RPS[pl]]
+	else :
+		return SCORES["lose"] + SCORES[PL_TO_RPS[pl]]
+
+def getPlayerScore_bonus(op, res) :
+
 	
 
 def day2() :
@@ -36,8 +55,11 @@ def day2() :
 		playerScore += getPlayerScore(line.split()[0], line.split()[1])
 	print(playerScore)
 
-def	main() :
-	day2()
+def day2_bonus() :
+
+	playerScore = 0
+	for line in open("input.txt", "r") :
+
 
 if __name__ == "__main__" :
-	main()
+	day2()
